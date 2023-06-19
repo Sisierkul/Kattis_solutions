8c4326fc32ecc47fea6fc16bce54870d1062be4a
Sisierkul <136910778+Sisierkul@users.noreply.github.com>
2023-06-19 09:40:17 +0200

    Update Exam_manipulation.py

--- Exam_manipulation.py
+++ Exam_manipulation.py
@@ -1,1 +1,16 @@
-
+N=int(input())
+myAnswers = input()
+hisAnswers = input()
+diffAnswers = 0
+sameAnswers = 0
+for i in range(len(myAnswers)):
+    if myAnswers[i] != hisAnswers[i]:
+        diffAnswers+=1
+    else:
+        sameAnswers+=1
+if sameAnswers == N:
+    print(len(myAnswers))
+elif sameAnswers > N:
+    print(len(myAnswers) - sameAnswers + N)
+else:
+    print(len(myAnswers) - N + sameAnswers)
